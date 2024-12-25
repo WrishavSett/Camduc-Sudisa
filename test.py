e6fa2f263ecdbd196e7dda38652e0e06718a0f74
WrishavSett <wrishavsett2912@gmail.com>
2024-12-25 10:10:18 +0530

    Copy of Camduc

---
+++ test.py
@@ -0,0 +1,43 @@
+import cv2
+
+from ultralytics import solutions
+
+# rtsp_stream = "rtsp://localhost:18554/mystream"
+rtsp_stream = "D:/SUDISA/recordings/Nov-19-2024/recording_20241119_112349.mp4"
+cap = cv2.VideoCapture(rtsp_stream)
+assert cap.isOpened(), "Error reading video file"
+w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
+
+# Define region points
+region_points = [(1250, 0), (1250, 1440)]  # For line counting
+# region_points = [(20, 400), (1080, 400), (1080, 360), (20, 360)]  # For rectangle region counting
+# region_points = [(20, 400), (1080, 400), (1080, 360), (20, 360), (20, 400)]  # For polygon region counting
+
+# Video writer
+video_writer = cv2.VideoWriter("object_counting_output.mp4", cv2.VideoWriter_fourcc(*"mp4v"), fps, (int(w/2), int(h/2)))
+
+# Init ObjectCounter
+counter = solutions.ObjectCounter(
+    show=True,  # Display the output
+    region=region_points,  # Pass region points
+    model="SudisaItem12K.pt",  # model="yolo11n-obb.pt" for object counting using YOLO11 OBB model.
+    # classes=[0, 2],  # If you want to count specific classes i.e person and car with COCO pretrained model.
+    show_in=True,  # Display in counts
+    show_out=True,  # Display out counts
+    line_width=2,  # Adjust the line width for bounding boxes and text display
+)
+
+# Process video
+frame_counter=0
+while cap.isOpened():
+    success, im0 = cap.read()
+    frame_counter+=1
+    if not success:
+        print("Video frame is empty or video processing has been successfully completed.")
+        break
+    im0 = counter.count(im0)
+    video_writer.write(im0)
+
+cap.release()
+video_writer.release()
+cv2.destroyAllWindows()
